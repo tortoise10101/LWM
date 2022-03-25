e315a087402c60a861084cda1ff5edebb7e4cba6
Tor <tor.one10101@gmail.com>
2022-03-26 01:14:34 +0900

    add ReplyBuffer

---
+++ worldmodel/replay_buffer.py
@@ -0,0 +1,54 @@
+import numpy as np
+import torch
+
+
+class ReplayBuffer(object):
+    def __init__(self, capacity, observation_shape, action_dim):
+        self.capacity = capacity
+
+        self.observations = np.zeros(
+            (capacity, *observation_shape), dtype=np.uint8)
+        self.actions = np.zeros((capacity, action_dim), dtype=np.float32)
+        self.rewards = np.zeros((capacity, 1), dtype=np.float32)
+        self.done = np.zeros((capacity, 1), dtype=np.bool)
+
+        self.index = 0
+        self.is_filled = False
+
+    def push(self, observation, action, reward, done):
+        self.observations[self.index] = observation
+        self.actions[self.index] = action
+        self.rewards[self.index] = reward
+        self.done[self.index] = done
+
+        if self.index == self.capacity - 1:
+            self.is_filled = True
+        self.index = (self.index + 1) % self.capacity
+
+    def sample(self, batch_size, chunk_length):
+        episode_borders = np.where(self.done)[0]
+        sampled_indexes = []
+        for _ in range(batch_size):
+            cross_border = True
+            while cross_border:
+                initial_index = np.random.randint(len(self) - chunk_length + 1)
+                final_index = initial_index + chunk_length - 1
+                cross_border = np.logical_and(
+                    initial_index <= episode_borders,
+                    episode_borders < final_index).any()
+            sampled_indexes += list(range(initial_index, final_index + 1))
+
+        sampled_observations = self.observations[sampled_indexes].reshape(
+            batch_size, chunk_length, *self.observations.shape[1:])
+        sampled_actions = self.actions[sampled_indexes].reshape(
+            batch_size, chunk_length, self.actions.shape[1])
+        sampled_rewards = self.rewards[sampled_indexes].reshape(
+            batch_size, chunk_length, 1)
+        sampled_done = self.done[sampled_indexes].reshape(
+            batch_size, chunk_length, 1)
+
+        return sampled_observations, sampled_actions, \
+            sampled_rewards, sampled_done
+
+    def __len__(self):
+        return self.capacity if self.is_filled else self.index
